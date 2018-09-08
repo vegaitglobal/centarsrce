b2a932f5e7a25766b1baf7ff87a277a0b7d9becb
Bojan Čoka <bojancoka1@protonmail.com>
2018-09-09 00:11:04 +0200

    Add footer links to external application handlers

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,26 +1,44 @@
 import React from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
+import { email, phonecall, web } from "react-native-communications";
 import styles from "./styles";
 
 export default class Footer extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity style={styles.btnCon}>
+        <TouchableOpacity
+          style={styles.btnCon}
+          onPress={() => phonecall("0800300303", true)}
+        >
           <Image
             source={require("../../images/poziv.png")}
             style={styles.callIcon}
           />
           <Text>Poziv</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.btnCon}>
+        <TouchableOpacity
+          style={styles.btnCon}
+          onPress={() =>
+            email(
+              "vanja@centarsrce.org",
+              undefined,
+              undefined,
+              undefined,
+              undefined
+            )
+          }
+        >
           <Image
             source={require("../../images/mail.png")}
             style={styles.mailIcon}
           />
           <Text>Email</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.btnCon}>
+        <TouchableOpacity
+          style={styles.btnCon}
+          onPress={() => web("https://centarsrce.org/")}
+        >
           <Image
             source={require("../../images/chat.png")}
             style={styles.chatIcon}
