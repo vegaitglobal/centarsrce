2d3dfbfb15d079c0cb536588b73e3bb7dbf42932
Milan Deket <milandeket@gmail.com>
2018-10-16 17:13:24 +0200

    Added open settings package

--- android/app/src/main/java/com/centarsrce/MainApplication.java
+++ android/app/src/main/java/com/centarsrce/MainApplication.java
@@ -3,6 +3,7 @@ package com.centarsrce;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.opensettings.OpenSettingsPackage;
 import com.rnfs.RNFSPackage;
 import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
 import org.reactnative.camera.RNCameraPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new OpenSettingsPackage(),
             new RNFSPackage(),
             new ReactNativeDocumentPicker(),
             new RNCameraPackage(),
